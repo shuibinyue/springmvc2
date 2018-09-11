b44922966f668d547705e6956f3d77deeecb54a1
Administrator <24252221832@qq.com>
2018-09-11 11:27:09 +0800

    SpringMVC拦截器的使用

---
+++ springmvc5/src/main/java/cap/controller/TestController.java
@@ -0,0 +1,13 @@
+package cap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class TestController {
+    @RequestMapping(value = "test")
+    public String test() {
+        return "/user/list";
+    }
+}
